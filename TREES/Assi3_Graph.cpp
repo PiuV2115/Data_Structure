8a0e3bffd02b89b326fac5c81f78172de5197063
Priya Kshirsagar <144653180+PiuV2115@users.noreply.github.com>
2025-03-18 10:56:21 +0530

    Update Assi3_Graph.cpp

--- TREES/Assi3_Graph.cpp
+++ TREES/Assi3_Graph.cpp
@@ -16,33 +16,31 @@ public:
 class List
 {
 public:
-	Node *arr[100],*track[100];
+	Node *arr[100];
 	List(int v)
 	{
 		for(int i=0;i<v;i++)
 		{
 			arr[i]=nullptr;
-			track[i]=nullptr;
 		}
 	}
 	void Edge(int e)
 	{
 	string m,n;
 	int d;
-	for(int i=0;i<2*e;i+2)
+	for(int i=0;i<2*e;i+=2)
 		{
 				cout<<i+1<<"] Enter (Source , Destination) : ";
 				cin>>m>>n;
 				cout<<"Distance Between ("<<m<<" , "<<n<<") :" ;
 				cin>>d;
+				int j=i+1;
 				Node *temp=new Node(n,d);
-				arr[i]->city=m;
 				temp->next=arr[i];
 				arr[i]=temp;
 				Node *temp1=new Node(m,d);
-				arr[i+1]->city=n;
-				temp1->next=arr[i+1];
-				arr[i+1]=temp1;
+				temp1->next=arr[j];
+				arr[j]=temp1;
    		}
 	}
 	
@@ -68,9 +66,6 @@ class Matrix
 	public:
 	int a[10][10];
 	string c[10]; 
-	char [26]={'a','b','c','d', 'e', 'f', 'g', 'h', 'i', 'j', 
-                        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-                        'u', 'v', 'w', 'x', 'y', 'z'};
 	int dist;
 	Matrix(int v,string ch[10])
 	{
@@ -84,36 +79,45 @@ class Matrix
 		}
 	}
 
-void accept(int edge)
+void accept(int edge,string alpha[10],int v)
 {
-	s m,n;
+	string m,n;
 	for(int i=0;i<edge;i++)
 	{
 		cout<<i+1<<"] Path [source , Destination] : ";
 		cin>>m>>n;
 		cout<<"Enter Distance : ";
 		cin>>dist;
-		a[m][n]=dist;
-		a[n][m]=dist;
+		for(int i=0;i<v;i++)
+		{
+		    for(int j=0;j<v;j++)
+		    {
+		        if(alpha[i]==m&&alpha[j]==n)
+		        {
+	        	a[i][j]=dist;
+	        	a[j][i]=dist;
+		        }
+		    }
+		}
 	}
 }
 
 void display(int size)
 {
     int n=0,count=0,t=1;
+     for(int k=0;k<size;k++)
+		    {
+		        cout<<"\t  "<<c[k];
+		    }
+		    cout<<"\n";
 	for(int i=0;i<size;i++)
 	{
+	     cout<<c[i]<<" | ";
 		for(int j=0;j<size;j++)
 		{
-		    for(int k=0;k<size;k++)
-		    {
-		        cout<<"\t"<<c[k];
-		    }
-		    cout<<"\n"<<c[i]<<"\t";
-		    cout<<"[";
-			cout<<a[i][j]<<"\t";
+			cout<<a[i][j]<<"\t  ";
 		}
-		cout<<"]\n";
+		cout<<"|\n";
 	}
 }
 	
@@ -125,37 +129,37 @@ int main()
 {
 
 	int v,e;
-	string a[i]
+	string a[10];
 	int choice;
 	cout<<"Enter No. of Cities : ";
 	cin>>v;
 	cout<<"Enter No. of Paths : ";
 	cin>>e;
+	cout<<"Enter Names of the cities";
+            for(int i=0;i<v;i++)
+            {
+            cin>>a[i];
+            }
+	Matrix m(v,a);
 	List lt(v);
 	do{
-	cout<<"\n1.Adjacency Matrix Accept \n2.Adjacency Matriz Display\n3.Adjacency list Accept\n4.Adjacency List Display\nEnter Your Choice : ";
+	cout<<"\n1.Adjacency Matrix 2.Adjacency List \nEnter Your Choice : ";
 	cin>>choice;
 	switch(choice)
 	{
-		case 1: m.accept(e);
+		case 1: m.accept(e,a,v);
 			cout<<"\nAccpetion of Adjacecny Matrix is Done";
 			cout<<"\nDisplay of Adjacency Matrix : \n";
 			m.display(v);
 			break;
 			
 		case 2:
-		  	cout<<"Enter Names of the cities";
-            for(int i=0;i<v;i++)
-            {
-            cin>>a[i];
-            }
-	        Matrix m(v,a);
 		    lt.Edge(e);
 			lt.Display(v);
 			break;
 		
 		default:cout<<"Invalid Choice";
 	}
-	}while(choice!=5);
+	}while(choice!=3);
 
 }
